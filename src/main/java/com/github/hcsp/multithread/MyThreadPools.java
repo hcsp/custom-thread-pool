5c7b319fa9ffce09e459807a6754580e69d0557e
fengjb <835488325@qq.com>
2020-04-22 13:03:32 +0800

    修改队列长度20，最大线程数10

--- src/main/java/com/github/hcsp/multithread/MyThreadPools.java
+++ src/main/java/com/github/hcsp/multithread/MyThreadPools.java
@@ -8,8 +8,8 @@ public class MyThreadPools {
     // 任务等待队列大小为20，如果超过20继续往该线程池中提交任务，这些任务会被悄悄丢弃
     // 线程的名字为"MyThread"
     public static ExecutorService myThreadPool() {
-        return new ThreadPoolExecutor(10, 20, 0,
-                TimeUnit.SECONDS, new LinkedBlockingQueue<>(),
+        return new ThreadPoolExecutor(10, 10, 0,
+                TimeUnit.SECONDS, new LinkedBlockingQueue<>(20),
                 runnable -> {
                     Thread thread = new Thread(runnable);
                     thread.setName("MyThread");
